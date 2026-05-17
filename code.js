2440db10ee676060c794da6ae2a9467dce628297
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-05-16 19:46:12 -0700

    base

---
+++ code.js
@@ -0,0 +1,241 @@
+const input = document.getElementById('input');
+const logContainer = document.getElementById('log-container');
+const hostname = 'jshell';
+const cmdPrompt = `user@${hostname}$ `; document.getElementById('prompt').innerText = cmdPrompt;
+const startup = Date.now();
+let history = [];
+let historyIndex = -1;
+let directoryHandle;
+
+input.focus();
+
+function print(text, type = 'default', raw = false) {
+    const log = document.createElement('div');
+    if (raw) {
+        log.innerHTML = text;
+    } else {
+        log.innerText = text;
+    }
+    log.classList.add(`log`);
+    log.classList.add(`log-${type}`);
+    logContainer.appendChild(log);
+
+    window.scrollTo({
+        top: document.body.scrollHeight
+    });
+}
+
+print(`Run 'init' to initialize`);
+print(`Run 'help' for more help`);
+print(` `)
+
+const commands = {
+    /* =========================================== */
+    /* ==============[ random shit ]============== */
+    /* =========================================== */
+    clear: {
+        description: 'Clears the screen',
+        run: args => { logContainer.innerHTML = '' }
+    },
+    neofetch: {
+        description: 'Displays system info',
+        run: args => {
+            print( /*pad*/  `              user@${hostname}`);
+            print(String.raw`    ___       -----`);
+            print(String.raw`   |\  \      OS: ${navigator.userAgentData.platform || 'Unknown'}`);
+            print(String.raw`   \ \  \     Shell: JShell`);
+            print(String.raw` __ \ \  \    User Agent: ${navigator.userAgent}`);
+            print(String.raw`|\  \\_\  \   Resolution: ${window.innerWidth}x${window.innerHeight}`);
+            print(String.raw`\ \________\  CPU: ${navigator.hardwareConcurrency} cores`);
+            print(String.raw` \|________|  Color Depth: ${screen.colorDepth}`);
+            print(String.raw`              Memory: ${navigator.deviceMemory} GB`);
+            print( /*pad*/  `              \
+<span style='color:red'>██</span>\
+<span style='color:green'>██</span>\
+<span style='color:yellow'>██</span>\
+<span style='color:blue'>██</span>\
+<span style='color:magenta'>██</span>\
+<span style='color:cyan'>██</span>\
+<span style='color:white'>██</span>`, 'default', true)
+        }
+    },
+    help: {
+        description: 'Shows all commands',
+        run: args => {
+            for (const [command, info] of Object.entries(commands)) {
+                print(`${command} - ${info.description || 'Unknown'}`);
+            }
+        }
+    },
+    exec: {
+        description: 'Executes inline Javascript',
+        run: async args => {
+            if (args.length !== 0) {
+                const code = args.join(' ');
+
+                try {
+                    const result = await eval(code);
+                    print(result);
+                } catch (error) {
+                    print(error, 'error');
+                }
+            } else {
+                print('Not enough args', 'error');
+            }
+        }
+    },
+    echo: {
+        description: 'Echoes a string',
+        run: args => {
+            print(args.join(' '));
+        }
+    },
+    /* ===================================== */
+    /* ==============[ utils ]============== */
+    /* ===================================== */
+    uptime: {
+        description: 'Returns the uptime (in seconds)',
+        run: args => {
+            print(`${(Date.now() - startup) / 1000} seconds`);
+        }
+    },
+    ping: {
+        description: 'Pings a website',
+        run: async args => {
+            if (args.length !== 0) {
+                let url = args.join(' ');
+                url = /^(https?|ftp|wss?):\/\//i.test(url) ? url : 'https://' + url;
+
+                print('pinging...');
+
+                try {
+                    await fetch(url, { method: 'GET', mode: 'no-cors' });
+                    print('online');
+                } catch (e) {
+                    print('offline');
+                }
+            } else {
+                print('Not enough args', 'error');
+            }
+        }
+    },
+    /* ========================================== */
+    /* ==============[ filesystem ]============== */
+    /* ========================================== */
+    init: {
+        description: 'Initializes shit',
+        run: async args => {
+            directoryHandle = await window.showDirectoryPicker();
+            print(`Successfully connected to filesystem '${directoryHandle.name}'`);
+        }
+    },
+    ls: {
+        description: 'Lists files in a folder',
+        run: async args => {
+            if (directoryHandle) {
+                let fileList = [];
+                for await (const entry of directoryHandle.values()) {
+                    if (entry.kind === "file") {
+                        fileList.push(await entry.name);
+                    }
+                }
+                print(fileList.join('  '));
+            } else {
+                print('Must first initialize', 'error');
+            }
+        }
+    },
+    touch: {
+        description: 'Creates a blank file',
+        run: async args => {
+            if (directoryHandle) {
+                if (args.length !== 0) {
+                    const name = args.join(' ');
+                    const fileHandle = await directoryHandle.getFileHandle(name, { create: true });
+                    const writable = await fileHandle.createWritable();
+                    await writable.close();
+                } else {
+                    print('Not enough args', 'error');
+                }
+            } else {
+                print('Must first initialize', 'error');
+            }
+        }
+    },
+    cat: {
+        description: 'Lists files in a folder',
+        run: async args => {
+            if (directoryHandle) {
+                if (args.length !== 0) {
+                    const name = args.join(' ');
+                    const fileHandle = await directoryHandle.getFileHandle(name);
+
+                    print(fileHandle);
+                    console.log(fileHandle);
+                } else {
+                    print('Not enough args', 'error');
+                }
+            } else {
+                print('Must first initialize', 'error');
+            }
+        }
+    }
+}
+
+async function process(raw) {
+    const [command, ...args] = raw.split(' ');
+    input.value = ''
+    print(`${cmdPrompt}${raw}`);
+
+    if (command) {
+        if (commands[command]) {
+            commands[command].run(args);
+        } else {
+            print(`Unrecognized command '${command}'`, 'error');;
+        }
+    }
+}
+
+document.addEventListener('mouseup', () => {
+    input.focus();
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        history.push(input.value);
+        historyIndex = history.length;
+        process(input.value);
+    }
+
+    if (event.key === 'ArrowUp') {
+        if (historyIndex > 0) {
+            historyIndex--;
+            input.value = history[historyIndex];
+        }
+        event.preventDefault();
+    }
+
+    if (event.key === 'ArrowDown') {
+        if (historyIndex < history.length - 1) {
+            historyIndex++;
+            input.value = history[historyIndex];
+        } else {
+            historyIndex = history.length;
+            input.value = '';
+        }
+        event.preventDefault();
+    }
+
+    if (event.key === 'Tab') {
+        event.preventDefault();
+
+        const text = input.value;
+        const matches = Object.keys(commands).filter(name =>
+            name.startsWith(text)
+        );
+
+        if (matches.length === 1) {
+            input.value = matches[0];
+        }
+    }
+});
