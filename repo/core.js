97f04651e9142a4bbfe7d9443b44d9ba4d5578ac
Ryan Michael Keller <211752292+averagebagelenjoyer@users.noreply.github.com>
2026-05-21 13:33:13 -0700

    new canvas command

--- repo/core.js
+++ repo/core.js
@@ -28,7 +28,7 @@ async function exec(args) {
 
     try {
       const result = await eval(code);
-      process(['echo', 'stdout',  result]);
+      process(['echo', 'stdout', result]);
     } catch (error) {
       process(['echo', 'stderr', `${error}`]);
     }
@@ -45,4 +45,48 @@ function exit(args) {
 function uptime(args) {
   //DESCRIPTION=Returns the uptime (in seconds)
   process(['echo', 'stdout', `${(Date.now() - startup) / 1000} seconds`]);
+}
+
+function canvas(args) {
+  [name, ...args] = args;
+
+  const canvasWindow = window.open('', 'canvasWindow');
+
+  canvasWindow.document.write(`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>canvas</title>
+
+  <link rel="icon" type="image/png" href="./favicons/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="./favicons/favicon.svg" />
+  <link rel="shortcut icon" href="./favicons/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./favicons/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="JShell" />
+  <link rel="manifest" href="./favicons/site.webmanifest" />
+</head>
+
+<body>
+  <canvas id="canvas" width="100%" height="100%">Your browser does not support canvas.</canvas>
+</body>
+
+</html>
+`);
+
+  const canvas = canvasWindow.document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  console.log(context)
+
+  if (name === 'fillStyle') {
+    context.fillStyle = args.join(' ')
+  } else {
+    const func = context[name];
+    if (typeof func === "function") {
+      func.apply(context, args.map(arg => Number.isFinite(Number(arg)) ? Number(arg) : arg));
+    }
+  }
 }
